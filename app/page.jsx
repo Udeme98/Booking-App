ad1c6e417dea6011b174107c9b78911e376bf6b6
Udeme98 <Darealudeme1@gmail.com>
2024-10-31 10:56:42 +0100

    roomCard component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
+import RoomCard from "./components/RoomCard";
+import rooms from "../data/rooms.json";
+
 export default function Home() {
   return (
-    <div>
-      <h2>Booking App</h2>
-    </div>
+    <>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No room available</p>
+      )}
+    </>
   );
 }
